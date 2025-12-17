d27511fc55d650392c637c3bfd8b9ef3e1bb305b
QinyRui <eqinivd19@163.com>
2025-12-18 01:44:02 +0800

    更新 BoxJsCors.js

--- BoxJsCors.js
+++ BoxJsCors.js
@@ -1,11 +1,16 @@
-// BoxJs 跨域重写脚本
-const res = {
-    headers: {
-        ...$response.headers,
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization"
-    },
-    body: $response.body
-};
-$done(res);
+// BoxJs 跨域重写脚本（完整匹配版）
+const match = $request.url.indexOf("boxjs.com/api/boxjs/get") > -1;
+if (match) {
+    const res = {
+        headers: {
+            ...$response.headers,
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization"
+        },
+        body: $response.body
+    };
+    $done(res);
+} else {
+    $done($response);
+}
