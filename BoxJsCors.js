b726ddb575aac88b3e8663d70c1340c0441836f3
QinyRui <eqinivd19@163.com>
2026-01-07 09:12:49 +0800

    更新 BoxJsCors.js

--- BoxJsCors.js
+++ BoxJsCors.js
@@ -1,11 +1,10 @@
-/* BoxJS跨域通用脚本（米游社/九号共用） */
 const res = {
-    headers: {
-        ...$response.headers,
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type"
-    },
-    body: $response.body
+  headers: {
+    ...$response.headers,
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type"
+  },
+  body: $response.body
 };
 $done(res);
